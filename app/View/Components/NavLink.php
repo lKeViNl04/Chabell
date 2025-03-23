0c1bded81627358f770e1f6161b0ee273b5fe9fb
lKeViNl04 <flores.kevin2972@gmail.com>
2025-03-23 17:07:46 -0300

    Update and rename navLink.php to NavLink.php

--- app/View/Components/NavLink.php
+++ app/View/Components/NavLink.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class navLink extends Component
+class NavLink extends Component
 {
     public string $routs;
     /**
